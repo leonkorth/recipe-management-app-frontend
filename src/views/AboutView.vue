721db463f42c70e592f23874023c69a38544f0d6
Quotchen <mailtomau@gmx.de>
2022-07-04 21:28:22 +0200

    finished about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="about text-center">
-    <h1>Hi,</h1>
-    <p>Willkommen zu unserer kleinen interaktiven Rezeptsammlung,<br>die wir im Rahmen des Kurses Webtechnologien an der HTW Berlin erstellt haben</p>
+    <br>
+    <h1>Über dieses Projekt</h1>
+    <p class="info-p info-p-narrow">Willkommen zu unserer kleinen interaktiven Rezeptsammlung, die wir im Rahmen des Kurses Webtechnologien an der HTW Berlin erstellt haben</p>
     <h3>Lass dich inspirieren...</h3>
-    <div id="text-field-middle">
-      <p>Unter dem Reiter <router-Link class="Link" to="/ingredients">Zutaten</router-Link>
+    <div class="text-field-middle">
+      <p class="info-p">Unter dem Reiter <router-Link class="Link" to="/ingredients">Zutaten</router-Link>
         kannst du alle Zutaten sehen, die bisher angelegt wurden. Dort kannst du auch auf einen Blick sehen,
         ob diese vegetarisch oder vegan sind. Nach diesen Kriterien kannst du auch über die Radio-Buttons filtern. Unter
         <router-link class="Link" to="/recipes">Rezepte</router-link>
@@ -13,14 +14,29 @@
         Hinweis zum Ernährungsstil und der Anzahl der Portionen, für die das Rezept ausgelegt ist. Ein Klick auf
         "Rezept ansehen" zeigt dir zudem die Anleitung und benötigten Zutaten zum Nachkochen.
       </p>
-      <p>Sowohl Zutaten als auch Rezepte kannst ganz einfach mithilfe der Suchleisten durchsuchen.
+      <p class="info-p">Sowohl Zutaten als auch Rezepte kannst ganz einfach mithilfe der Suchleisten durchsuchen.
         Sobald du etwas eingetippt hast, wird dir angezeigt, welche Namen deiner Suche.
       </p>
     </div>
 
-    <h3>...
-      und teile deine Kreativität
-    </h3>
+    <h3>... und teile deine Kreativität</h3>
+    <div class="text-field-middle">
+      <p class="info-p">
+        Lege über "Rezept hinzufügen" ein neues Rezept an, indem du einen Namen und eine Anleitung in die
+        entsprechenden Textfelder schreibst und die Anzahl an Portionen angibst. Wähle eine erste Zutat aus dem
+        Drop-Down-Menü und klicke auf "Ausgewählte Zutat hinzufügen", um Menge und Einheit für diese Zutat
+        angeben zu können. Auf die selbe Weise kannst du alle Zutaten anlegen und "Rezept speichern", sobald es
+        komplett ist.
+      </p>
+      <p class="info-p">
+        Solltest du mit einem deiner Rezepte nicht mehr zufrieden sein, kannst du es über "Rezept löschen"
+        einfach entfernen. Fehlt für dein Rezept noch eine Zutat, kannst du sie über "Zutat hinzufügen" ergänzen.
+        Gib hierfür einfach einen Namen und die Information, ob sie vegetarisch oder vegan ist, an.
+        <br> Für ausführlichere Informationen auf Englisch, sieh dir unser ReadMe auf
+        <a href="https://github.com/leonkorth/recipe-management-app-frontend">GitHub</a> an.
+      </p>
+
+    </div>
   </div>
 </template>
 
@@ -28,9 +44,22 @@
 
 </script>
 
-<style>
-#text-field-middle{
-  margin-right: 200px;
-  margin-left: 200px;
+<style lang="scss">
+@import "src/assets/style/overall.scss";
+
+p.info-p{
+  padding-right: 200px;
+  padding-left: 200px;
+  &-narrow {
+     padding-right: 300px;
+     padding-left: 300px;
+   }
+}
+router-link {
+
+  link: $nav-text;
+}
+a:any-link {
+  color: #284a4a;
 }
 </style>
